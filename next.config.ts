35d88c9c81670c03d9dba2621178e8372b2b9661
Abram2173 <127432561+Abram2173@users.noreply.github.com>
2025-10-15 16:35:59 -0600

    Añadiendo build estático para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Desactiva optimización de imágenes para static export
+  },
 };
 
 export default nextConfig;
